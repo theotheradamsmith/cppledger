4efcdfd4f6eaea663e8aa8c2778d722b5b3a4628
Adam Smith <adams@cipafilter.com>
2017-02-01 17:26:38 -0600

    0016: Continue work on background

--- estate.cpp
+++ estate.cpp
@@ -7,6 +7,7 @@
 #include <Wt/WVBoxLayout>
 #include <Wt/WHBoxLayout>
 #include <Wt/WApplication>
+#include <Wt/WBorderLayout>
 
 #include "estate.h"
 #include "menu_item.h"
@@ -17,32 +18,58 @@ estate::estate() {
 	WText *title = new WText("<h1>Ledger of Your Estate</h1>");
 	addWidget(title);
 
+	WContainerWidget *container = new WContainerWidget(this);
+	container->setHeight(600);
+	container->setStyleClass("backdrop");
+
+	WBorderLayout *layout = new WBorderLayout();
+	container->setLayout(layout);
+
+	const char *cell = "{1} item";
+	Wt::WText *item = new Wt::WText(Wt::WString(cell).arg("North"));
+	item->setStyleClass("blue-box");
+	layout->addWidget(item, Wt::WBorderLayout::North);
+
+	item = new Wt::WText(Wt::WString(cell).arg("East"));
+	item->setStyleClass("blue-box");
+	layout->addWidget(item, Wt::WBorderLayout::East);
+
+	item = new Wt::WText(Wt::WString(cell).arg("South"));
+	item->setStyleClass("blue-box");
+	layout->addWidget(item, Wt::WBorderLayout::South);
+
+	item = new Wt::WText(Wt::WString(cell).arg("Center"));
+	item->setStyleClass("green-box");
+	layout->addWidget(item, Wt::WBorderLayout::Center);
+
 	contentsStack_ = new WStackedWidget();
 	WAnimation animation(WAnimation::Fade, WAnimation::Linear, 200);
 	contentsStack_->setTransitionAnimation(animation, true);
+
+	WMenu *menu = new WMenu(contentsStack_);
+	menu->setInternalPathEnabled();
+	menu->setInternalBasePath("/");
+	addToMenu(menu, "Accounts", new MenuItem());
+	addToMenu(menu, "Envelopes", new MenuItem());
+	addToMenu(menu, "Summary", new MenuItem());
 
 	navigation_ = new WNavigationBar();
 	navigation_->addStyleClass("navigation");
 	navigation_->setTitle("Estate Ledger", "http://localhost:9090");
 	navigation_->setResponsive(true);
-
-	WMenu *menu = new WMenu(contentsStack_);
-	menu->setInternalPathEnabled();
-	menu->setInternalBasePath("/");
-
-	addToMenu(menu, "Accounts", new MenuItem());
-	addToMenu(menu, "Envelopes", new MenuItem());
-	addToMenu(menu, "Summary", new MenuItem());
-
 	navigation_->addMenu(menu);
 
-	WVBoxLayout *layout = new WVBoxLayout(this);
-	layout->addWidget(navigation_);
-	layout->addWidget(contentsStack_, 1);
-	layout->setContentsMargins(0, 0, 0, 0);
+	WVBoxLayout *nav_layout = new WVBoxLayout();
+	nav_layout->addWidget(navigation_);
+	nav_layout->addWidget(contentsStack_, 1);
+	nav_layout->setContentsMargins(0, 0, 0, 0);
+
+	WContainerWidget *nav_container = new WContainerWidget(this);
+	nav_container->setStyleClass("blue-box");
+	nav_container->setLayout(nav_layout);
+	layout->addWidget(nav_container, Wt::WBorderLayout::West);
 
 	WApplication::instance()->internalPathChanged().connect(this, &estate::handleInternalPath);
-
 }
 
 WMenuItem *estate::addToMenu(WMenu *menu, const WString &name, MenuItem *topic) {
