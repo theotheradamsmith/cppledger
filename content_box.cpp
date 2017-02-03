b98a4d87c19535a8ce7ad56f795766c3e023bd4b
Adam Smith <adams@cipafilter.com>
2017-02-03 14:59:58 -0600

    0019: Display envelope data

--- content_box.cpp
+++ content_box.cpp
@@ -1,25 +1,42 @@
 #include <list>
+#include <Wt/WHBoxLayout>
 #include <Wt/WText>
+#include <Wt/WVBoxLayout>
 
 #include "content_box.h"
-#include "test.h"
 
 using namespace Wt;
 
-content_box::content_box(WContainerWidget *parent, database *db) {
+content_box::content_box(WContainerWidget *parent, database *db)
+	: database_connection(db)
+{
 	WContainerWidget *container = new WContainerWidget(parent);
 	container->setStyleClass("green-box");
 
-	list<account *> acc_list;
-	db->load_data(acc_list);
+	WVBoxLayout *vbox = new WVBoxLayout();
+	container->setLayout(vbox);
+
+	db->load_data(account_list);
 
 	const char *acc_info = "<p>Account: {1}</p>"
 						   "<p>Total Balance: {2}</p>"
 						   "<p>Avail Balance: {3}</p>"
 						   "<p>Envelopes: {4}</p>";
 
-	account acc = *(acc_list.front());
+	account acc = *(account_list.front());
 	WText *item = new WText(WString(acc_info).arg(acc.get_name()).arg(std::to_string(acc.get_total_account_balance())).arg(std::to_string(acc.get_available_balance())).arg(std::to_string(acc.get_number_of_envelopes())));
-	container->addWidget(item);
+	item->setStyleClass("blue-box");
+	vbox->addWidget(item, 1);
 
+	WHBoxLayout *hbox = new WHBoxLayout();
+	vbox->addLayout(hbox);
+
+	const char *env_info = "<p>{1} - Envelope: {2}</p>"
+						   "<p>Balance: {3}</p>";
+
+	for (auto env : acc.envelopes) {
+		item = new WText(WString(env_info).arg(std::to_string(env->get_id())).arg(env->get_name()).arg(std::to_string(env->get_available_balance())));
+		item->setStyleClass("blue-box");
+		hbox->addWidget(item);
+	}
 }
--- content_box.h
+++ content_box.h
@@ -1,9 +1,15 @@
 #pragma once
 
+#include <list>
 #include <Wt/WContainerWidget>
+
+#include "account.h"
 #include "database.h"
 
 class content_box {
 	public:
 		content_box(Wt::WContainerWidget *parent, database *db);
+	private:
+		database *database_connection;
+		list<account *> account_list;
 };
